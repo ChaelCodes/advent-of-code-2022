d62d913452c92cdf20495747095e3fdce334ec97
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2022-12-04 19:02:56 +0000

    Scaffold Day 4

---
+++ solutions/day_04.rb
@@ -0,0 +1,31 @@
+class Day04
+  attr_accessor :records, :part
+
+  def initialize(part: 1)
+    self.records = inputs
+    self.part = part
+  end
+
+  def inputs
+    day_match = self.class.name.match(/Day(?<number>\d+)/)
+    File.readlines("./inputs/day_#{day_match[:number]}.txt").map(&:chomp)
+  end
+
+  def solve_part_1
+    records.count
+  end
+
+  def solve_part_2
+    records.count
+  end
+
+  def solve
+    if part == 1
+      solve_part_1
+    else
+      solve_part_2
+    end
+  end
+end
+
+Day04.new.solve
