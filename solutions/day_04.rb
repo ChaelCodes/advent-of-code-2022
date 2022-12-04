4ed81f1fab84b2de3114cbd1d66f9b379f64c0b1
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2022-12-04 19:44:07 +0000

    Part 1 of Day 4

--- solutions/day_04.rb
+++ solutions/day_04.rb
@@ -1,8 +1,8 @@
 class Day04
-  attr_accessor :records, :part
+  attr_accessor :assignment_pairs, :part
 
   def initialize(part: 1)
-    self.records = inputs
+    self.assignment_pairs = inputs
     self.part = part
   end
 
@@ -11,8 +11,15 @@ class Day04
     File.readlines("./inputs/day_#{day_match[:number]}.txt").map(&:chomp)
   end
 
+  def overlapping_assignments(assignment_pair)
+    assignments = assignment_pair.match /(?<start_1>\d+)-(?<end_1>\d+),(?<start_2>\d+)-(?<end_2>\d+)/
+    assignment_1 = (assignments[:start_1].to_i..assignments[:end_1].to_i)
+    assignment_2 = (assignments[:start_2].to_i..assignments[:end_2].to_i)
+    assignment_1.cover?(assignment_2) || assignment_2.cover?(assignment_1)
+  end
+
   def solve_part_1
-    records.count
+    assignment_pairs.count(&method(:overlapping_assignments))
   end
 
   def solve_part_2
