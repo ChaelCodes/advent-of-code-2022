215b1ae3e7fc35afc8357a5a52cc714b74f725bd
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2022-12-11 16:17:25 +0000

    Complete Part 1 of Day 5

--- solutions/day_05.rb
+++ solutions/day_05.rb
@@ -1,8 +1,13 @@
 class Day05
-  attr_accessor :records, :part
+  attr_accessor :crates, :instructions, :part
 
   def initialize(part: 1)
-    self.records = inputs
+    records = inputs
+    self.crates = {}
+    records[0...9].each.with_index do |stack, index|
+      crates[index + 1] = stack.chars
+    end
+    self.instructions = records[(20..)]
     self.part = part
   end
 
@@ -11,12 +16,27 @@ class Day05
     File.readlines("./inputs/day_#{day_match[:number]}.txt", chomp: true)
   end
 
+  def move_crate(instruction)
+    parsed_instruction = instruction.match(/move (?<crate_count>\d+) from (?<from_stack>\d+) to (?<to_stack>\d+)/)
+    crate_count, from_stack, to_stack = parsed_instruction[:crate_count].to_i, parsed_instruction[:from_stack].to_i, parsed_instruction[:to_stack].to_i
+    crate_count.times { crates[to_stack] << crates[from_stack].pop }
+  end
+
+  def run_instructions
+    instructions.each(&method(:move_crate))
+  end
+
+  def answer
+    crates.values.map(&:last).join
+  end
+
   def solve_part_1
-    records.count
+    run_instructions
+    answer
   end
 
   def solve_part_2
-    records.count
+    instructions.count
   end
 
   def solve
@@ -27,5 +47,3 @@ class Day05
     end
   end
 end
-
-Day05.new.solve
--- spec/day_05_spec.rb
+++ spec/day_05_spec.rb
@@ -1,18 +1,67 @@
 RSpec.describe Day05 do
   let(:solution) { described_class.new }
 
-  describe "#inputs" do
-    subject { solution.inputs }
+  describe "#initialize" do
+    subject { solution }
 
-    xit { expect(subject.first).to eq "" }
-    it { expect(subject.count).to eq 0 }
+    it "parses crates" do
+      expect(solution.crates[1]).to eq %w[B S V Z G P W]
+      expect(solution.crates[2]).to eq %w[J V B C Z F]
+      expect(solution.crates[3]).to eq %w[V L M H N Z D C]
+      expect(solution.crates[4]).to eq %w[L D M Z P F J B]
+      expect(solution.crates[5]).to eq %w[V F C G J B Q H]
+      expect(solution.crates[6]).to eq %w[G F Q T S L B]
+      expect(solution.crates[7]).to eq %w[L G C Z V]
+      expect(solution.crates[8]).to eq %w[N L G]
+      expect(solution.crates[9]).to eq %w[J F H C]
+    end
+
+    it "has first instruction" do
+      expect(solution.instructions.first).to eq "move 5 from 4 to 7"
+    end
+
+    it "has last instruction" do
+      expect(solution.instructions.last).to eq "move 1 from 8 to 1"
+    end
+
+    it "has all instructions" do
+      expect(solution.instructions.count).to eq 501
+    end
+  end
+
+  describe "#move_crate" do
+    subject { solution.move_crate(instruction) }
+    let(:instruction) { "move 1 from 2 to 1" }
+
+    before do
+      solution.crates = {
+        1 => %w[Z N],
+        2 => %w[M C D],
+        3 => %w[P]
+      }
+    end
+
+    it "moves the crate" do
+      subject
+      expect(solution.crates).to eq({
+        1 => %w[Z N D],
+        2 => %w[M C],
+        3 => %w[P]
+      })
+    end
+  end
+
+  describe "#answer" do
+    subject { solution.answer }
+
+    it { is_expected.to eq "WFCBHBVGC" }
   end
 
   describe "#solve" do
     subject { solution.solve }
 
     context "for part 1" do
-      it { is_expected.to eq 0 }
+      it { is_expected.to eq "VJSFHWGFT" }
     end
 
     xcontext "for part 2" do
