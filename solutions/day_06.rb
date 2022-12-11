abc2e80736359f6424c203676c0cabb35cb22fa5
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2022-12-11 17:19:38 +0000

    Part 1 for Day 6

--- solutions/day_06.rb
+++ solutions/day_06.rb
@@ -1,18 +1,36 @@
 class Day06
-  attr_accessor :records, :part
+  attr_accessor :datastream, :part
 
-  def initialize(part: 1)
-    self.records = inputs
+  def initialize(part: 1, datastream: nil)
+    self.datastream = datastream || inputs
     self.part = part
   end
 
   def inputs
     day_match = self.class.name.match(/Day(?<number>\d+)/)
-    File.readlines("./inputs/day_#{day_match[:number]}.txt").map(&:chomp)
+    File.readlines("./inputs/day_#{day_match[:number]}.txt", chomp: true)[0]
+  end
+
+  def find_markers
+    markers = []
+    datastream.chars.each_cons(4) do |char_array|
+      if char_array.uniq.count == 4
+        markers << char_array.join
+      end
+    end
+    markers
+  end
+
+  def first_marker
+    find_markers.first
+  end
+
+  def index_of_marker(marker)
+    datastream.index(marker) + 4
   end
 
   def solve_part_1
-    records.count
+    index_of_marker(first_marker)
   end
 
   def solve_part_2
--- spec/day_06_spec.rb
+++ spec/day_06_spec.rb
@@ -1,18 +1,38 @@
 RSpec.describe Day06 do
-  let(:solution) { described_class.new }
+  let(:solution) { described_class.new(datastream: datastream) }
+  let(:datastream) { nil }
 
   describe "#inputs" do
     subject { solution.inputs }
 
-    xit { expect(subject.first).to eq "" }
-    it { expect(subject.count).to eq 0 }
+    it { expect(subject.chars.first(5)).to eq %w[m n l n v] }
+  end
+
+  describe "#first_marker" do
+    subject { solution.first_marker }
+
+    context "with sample 1" do
+      let(:datastream) { "mjqjpqmgbljsphdztnvjfqwrcgsmlb" }
+
+      it { is_expected.to eq "jpqm" }
+    end
+  end
+
+  describe "#index_of_marker" do
+    subject { solution.index_of_marker("jpqm") }
+
+    context "with sample 1" do
+      let(:datastream) { "mjqjpqmgbljsphdztnvjfqwrcgsmlb" }
+
+      it { is_expected.to eq 7 }
+    end
   end
 
   describe "#solve" do
     subject { solution.solve }
 
     context "for part 1" do
-      it { is_expected.to eq 0 }
+      it { is_expected.to eq 1816 }
     end
 
     xcontext "for part 2" do
