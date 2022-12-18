497b565def652695982db29b68409d31294d39d4
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2022-12-18 18:24:52 +0000

    Part 1 of Day 8

--- solutions/day_08.rb
+++ solutions/day_08.rb
@@ -1,18 +1,68 @@
 class Day08
-  attr_accessor :records, :part
+  attr_accessor :trees, :part, :visibility_map
 
   def initialize(part: 1)
-    self.records = inputs
+    self.trees = inputs
+    self.visibility_map = Array.new(99) { Array.new(99, '') }
     self.part = part
   end
 
   def inputs
     day_match = self.class.name.match(/Day(?<number>\d+)/)
-    File.readlines("./inputs/day_#{day_match[:number]}.txt", chomp: true)
+    File.readlines("./inputs/day_#{day_match[:number]}.txt", chomp: true).map(&:chars)
+  end
+
+  def parse_tree(tree)
+   tree.match(/(?<visible>[vi]?)(?<height>\d)/)
+  end
+
+  def tree_visibility
+    # West
+    trees.each.with_index do |tree_stand, stand_index|
+      tallest_tree = -1
+      tree_stand.each.with_index do |tree, tree_index|
+        if tree.to_i > tallest_tree
+          visibility_map[stand_index][tree_index] = 'v'
+          tallest_tree = tree.to_i
+        end
+      end
+    end
+
+    # East
+    trees.each.with_index do |tree_stand, stand_index|
+      tallest_tree = -1
+      tree_stand.reverse.each.with_index do |tree, tree_index|
+        if tree.to_i > tallest_tree
+          visibility_map[stand_index][-tree_index - 1] = 'v'
+          tallest_tree = tree.to_i
+        end
+      end
+    end
+
+    (0...trees.first.length).each do |tree_index|
+      vertical = trees.map { _1[tree_index] }
+      # South
+      tallest_tree = -1
+      vertical.each.with_index do |tree, stand_index|
+        if tree.to_i > tallest_tree
+          visibility_map[stand_index][tree_index] = 'v'
+          tallest_tree = tree.to_i
+        end
+      end
+      # North
+      tallest_tree = -1
+      vertical.reverse.each.with_index do |tree, stand_index|
+        if tree.to_i > tallest_tree
+          visibility_map[-stand_index - 1][tree_index] = 'v'
+          tallest_tree = tree.to_i
+        end
+      end
+    end
   end
 
   def solve_part_1
-    records.count
+    tree_visibility
+    visibility_map.sum { |row| row.count('v')}
   end
 
   def solve_part_2
--- spec/day_08_spec.rb
+++ spec/day_08_spec.rb
@@ -4,15 +4,30 @@ RSpec.describe Day08 do
   describe "#inputs" do
     subject { solution.inputs }
 
-    xit { expect(subject.first).to eq "" }
-    it { expect(subject.count).to eq 0 }
+    it { expect(subject.first).to eq %w[1 2 0 1 2 1 0 1 0 2 2 0 0 1 1 2 1 0 2 1 3 3 4 1 2 2 1 4 0 0 4 1 0 0 3 2 0 2 4 2 0 0 3 0 5 4 5 5 1 2 3 3 3 2 2 2 4 0 0 4 3 4 4 2 3 2 0 3 2 4 1 0 4 0 1 4 3 1 1 0 2 2 3 0 3 1 3 2 2 1 0 3 2 2 2 0 2 0 1] }
+    it { expect(subject.last).to eq %w[1 0 2 1 0 1 0 1 0 0 3 1 3 1 2 3 2 0 3 0 1 1 3 3 1 3 3 4 2 0 1 2 4 4 2 5 3 5 2 5 5 1 4 4 1 5 5 3 5 5 2 4 1 1 3 3 1 3 1 4 1 2 2 5 1 5 4 2 3 4 2 0 1 0 0 0 2 0 3 1 2 0 4 1 2 3 0 1 2 3 1 0 2 2 2 0 2 1 2] }
+    it { expect(subject.first.count).to eq 99 }
+    it { expect(subject.count).to eq 99 }
   end
 
   describe "#solve" do
     subject { solution.solve }
 
     context "for part 1" do
-      it { is_expected.to eq 0 }
+      it { is_expected.to eq 1816 }
+
+      context "with sample inputs" do
+        before do
+          solution.trees = %w[30373
+                              25512
+                              65332
+                              33549
+                              35390].map(&:chars)
+          solution.visibility_map = solution.trees.map(&:dup)
+        end
+
+        it { is_expected.to eq 21 }
+      end
     end
 
     xcontext "for part 2" do
