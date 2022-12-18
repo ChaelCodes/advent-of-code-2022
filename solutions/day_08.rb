be0f8f478e1d8d0dad01af6b6850643e06fbedc4
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2022-12-18 15:12:06 +0000

    Scaffold Day 8

---
+++ solutions/day_08.rb
@@ -0,0 +1,29 @@
+class Day08
+  attr_accessor :records, :part
+
+  def initialize(part: 1)
+    self.records = inputs
+    self.part = part
+  end
+
+  def inputs
+    day_match = self.class.name.match(/Day(?<number>\d+)/)
+    File.readlines("./inputs/day_#{day_match[:number]}.txt", chomp: true)
+  end
+
+  def solve_part_1
+    records.count
+  end
+
+  def solve_part_2
+    records.count
+  end
+
+  def solve
+    if part == 1
+      solve_part_1
+    else
+      solve_part_2
+    end
+  end
+end
